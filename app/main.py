4bacee90d909e7c52dc2fc884cd87c92b7507f0b
Mukhanbetzhusupova Malika Ulanovna <malika.m.ulanovna@gmail.com>
2025-11-01 23:01:38 +0100

    feat(api): register /init route in main app

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routes import search
+from app.routes import search,init
 
 app = FastAPI(title="Semantic Recipe Search API")
 
 app.include_router(search.router)
+app.include_router(init.router)
 
 @app.get("/")
 def read_root():
