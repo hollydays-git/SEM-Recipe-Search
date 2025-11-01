3275565f421ac85102a9b0f2b238d5ef367278ee
Mukhanbetzhusupova Malika Ulanovna <malika.m.ulanovna@gmail.com>
2025-11-01 22:57:19 +0100

    feat(api): connect /search endpoint to Qdrant semantic search

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,12 +1,31 @@
 from fastapi import APIRouter, Query
+from sentence_transformers import SentenceTransformer
+from app.core.qdrant_client import  qdrant
+from app.core.config import settings
 
 router = APIRouter(prefix="/search", tags=["search"])
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
 
 @router.get("/")
 def search_recipes(q: str = Query(..., description="Search query")):
-    # Mock response
+
+    #create embedding from user query
+    query_vector = model.encode(q).tolist()
+
+    #search in qdrant
+    search_result = qdrant.search(
+        collection_name=settings.QDRANT_COLLECTION,
+        query_vector=query_vector,
+        limit=5
+    )
+
+    #format result
     results = [
-        {"name": "Apple Pie", "similarity": 0.89},
-        {"name": "Spicy Malaysian Chicken", "similarity": 0.83},
+        {
+            "name": hit.payload.get("name", "Unknown"),
+            "score": hit.score
+        } for hit in search_result
     ]
+
     return {"query": q, "results": results}
